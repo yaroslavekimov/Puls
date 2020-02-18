3c0d7a46ac2eff84ab88748001383797b01511a7
Yaroslav Ekimov <YaroslavMur@mail.ru>
2020-02-18 12:13:15 +0300

    rewiews

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__item').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
